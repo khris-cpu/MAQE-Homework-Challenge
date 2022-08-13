df0b23464dacaea276329d4f5b26e054c6a0d204
Khris Bharmmano <khrisbharmmano@gmail.com>
2022-08-13 13:03:09 +0700

    Modefined index.hs but fail

--- index.js
+++ index.js
@@ -1,54 +1,71 @@
-var Xpos = 0;
-var Ypos = 0;
-var i = 0;
-const arr = [];
 var deg = 0;
-const test = "RW15RW1";
-n = test.length;
-number = typeof 42;
-string = typeof 'blubber';
-boolean = typeof true;
 
-// Loop
+var test = "W5RW5RW2RW1R";
+var i = 0;
+var n = test.length;
 
-while(i < n){
+// Types
 
-    if (test[i] == 'R'){
-        deg = deg + 90;
-        console.log(deg);
-    }else if (test[i] == 'L'){
-        def = deg - 90;
-        console.log(deg);
+var number = typeof 1;
+var string = typeof "Khris Bharmmano";
+var boolean = typeof true;
+var undefined = typeof test[9];
+
+
+while (i < n){
+    // console.log(test[i]);
+
+    // Direction
+    if(test[i] == "R"){
+        deg += 90;
+    }else if(test[i] == "L"){
+        deg -= 90;
     }
-    
-    if (test[i] == 'W'){
-        if (deg == 0){
-            Ypos = Ypos + test[i + 1];
-            console.log(Ypos)
-        }else if (deg == 180){
-            Ypos = Ypos - test[i + 1];
-            console.log(Ypos)
-        }else if (deg == 90){
-            Xpos = Xpos + test[i + 1]
-            console.log(Xpos)
-        }else if (deg == 270){
-            Xpos = Xpos - test[i + 1]
-            console.log(Xpos)
+
+    if (deg > 360){
+        deg = deg - 360
+    }
+
+    // Value of X Position and Y Postion
+
+    if (test[i] == "W"){
+        var j = i + 1;
+        var Xpos = 0;
+        var Ypos = 0;
+        console.log(`Number Position : ${j}`)
+        var num = 0;
+        while (test[j] == "R" || test[j] == "L"){
+            if (deg == 0 || deg == 360){
+                Ypos += test[j];
+                j += 1;
+            }else if (deg ==90){
+                Ypos += test[j];
+                j += 1;
+            }else if (deg == 180){
+                Ypos -= test[j];
+                j += 1;
+            }else if (deg == 270){
+                Xpos -= test[j];
+                j += 1;
+            }
         }
     }
-    i = i + 1
 
+    i +=1;
 }
-
-// Check Direction
 
 if (deg == 0 || deg == 360){
-    console.log("Direction : North");
+    console.log(`Direction : North`);
 }else if (deg == 90){
-    console.log("Direction : East");
+    console.log(`Direction : East`);
 }else if (deg == 180){
-    console.log("Direction : South");
+    console.log(`Direction : South`);
 }else if (deg == 270){
-    console.log("Direction : West");
+    console.log(`Direction : West`);
 }
-console.log(`Degrees : ${deg}`);
+
+console.log(deg);
+console.log(`Position : ${num}`);
+
+console.log(`X : ${Xpos}`);
+console.log(`Y : ${Ypos}`);
